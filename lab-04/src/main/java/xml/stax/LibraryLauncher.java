b6b58c4f73579c989e833d19655f16704007365d
Evdokimenko97 <evdokimenko_9797@mail.ru>
2021-10-26 18:46:54 +0300

    4 Лаборатория

--- lab-04/src/main/java/xml/stax/LibraryLauncher.java
+++ lab-04/src/main/java/xml/stax/LibraryLauncher.java
@@ -4,10 +4,13 @@ import xml.domain.Author;
 import xml.domain.Book;
 import xml.domain.Library;
 
-import javax.xml.stream.XMLStreamException;
+import javax.xml.namespace.QName;
+import javax.xml.stream.*;
+import javax.xml.stream.events.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashSet;
 import java.util.Iterator;
 
 /**
@@ -53,11 +56,70 @@ public class LibraryLauncher {
     }
 
     private static Library buildLibrary(final InputStream is) throws XMLStreamException {
-        // Write your code here
-        return null;
+        Library library = null;
+        Author author = new Author();
+        Book book = null;
+
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        try {
+            XMLEventReader reader = factory.createXMLEventReader(is);
+            while (reader.hasNext()) {
+                XMLEvent event = reader.nextEvent();
+
+                if (event.isStartElement()) {
+                    StartElement startElement = event.asStartElement();
+                    if (startElement.getName().getLocalPart().equals("books")) {
+                        library = new Library();
+                        library.setAuthors(new HashSet<Author>());
+                        library.setBooks(new HashSet<Book>());
+                    } else if (startElement.getName().getLocalPart().equals("book")) {
+                        book = new Book();
+                        book.setAuthors(new HashSet<Author>());
+                    } else if (startElement.getName().getLocalPart().equals("author")) {
+                        author = new Author();
+                        Attribute attribute = startElement.getAttributeByName(new QName("name"));
+                        author.setName(attribute.getValue());
+                        book.getAuthors().add(author);
+                    } else if (startElement.getName().getLocalPart().equals("title")) {
+                        event = reader.nextEvent();
+                        book.setTitle(event.asCharacters().getData());
+                    }
+                }
+                if (event.isEndDocument()) {
+                    EndElement endElement = event.asEndElement();
+                    if (endElement.getName().getLocalPart().equals("book")) {
+                        library.getBooks().add(book);
+                    }
+                    if (endElement.getName().getLocalPart().equals("author")) {
+                        library.getAuthors().add(author);
+                    }
+                }
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return library;
     }
 
     private static void writeLibrary(final Library library, final OutputStream os) throws XMLStreamException {
-        // Write your code here
+        XMLOutputFactory factory = XMLOutputFactory.newInstance();
+        XMLStreamWriter writer = factory.createXMLStreamWriter(os);
+        writer.writeStartDocument();
+        writer.writeStartElement("library");
+        writer.writeStartElement("books");
+        for (Book book : library.getBooks()) {
+            writer.writeStartElement("book");
+            for (Author author : book.getAuthors()) {
+                writer.writeEmptyElement("author");
+                writer.writeAttribute("name", author.getName());
+            }
+            writer.writeStartElement("title");
+            writer.writeCharacters(book.getTitle());
+            writer.writeEndElement();
+            writer.writeEndElement();
+        }
+        writer.writeEndElement();
+        writer.writeEndElement();
     }
 }
